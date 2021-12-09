2b4cf2119a9c528315aeefe47e7b839cca780045
rookie-leo <proleonarpa@gmail.com>
2021-12-09 17:18:32 -0300

    Criado classe de dominio Categoria

--- src/main/java/com/leonardo/cursomc/controller/CategoriaController.java
+++ src/main/java/com/leonardo/cursomc/controller/CategoriaController.java
@@ -1,15 +1,28 @@
 package com.leonardo.cursomc.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.leonardo.cursomc.model.Categoria;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
 
 	@GetMapping()
-	public String listar() {
-		return "REST está funcionado";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informatica");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 }
