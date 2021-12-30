53ca6d9ee378b13f4599e5b21de501adc4a5e0d0
rookie-leo <proleonarpa@gmail.com>
2021-12-30 14:31:18 -0300

    Implementado endpoint de inserção de nova categoria

--- src/main/java/com/leonardo/cursomc/controller/CategoriaController.java
+++ src/main/java/com/leonardo/cursomc/controller/CategoriaController.java
@@ -1,13 +1,17 @@
 package com.leonardo.cursomc.controller;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leonardo.cursomc.model.Categoria;
 import com.leonardo.cursomc.repositories.CategoriaRepository;
@@ -20,7 +24,7 @@ public class CategoriaController {
 	private CategoriaRepository repository;
 
 	@GetMapping("/{id}")
-	public ResponseEntity<Categoria> buscar(@PathVariable("id") Long id) {
+	public ResponseEntity<Categoria> search(@PathVariable("id") Long id) {
 		Optional<Categoria> categoria = repository.findById(id);
 
 		if (categoria.isPresent()) {
@@ -28,6 +32,17 @@ public class CategoriaController {
 		}
 
 		return ResponseEntity.status(404).build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		repository.save(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
